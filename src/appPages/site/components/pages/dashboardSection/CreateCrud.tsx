920cc8f5cdaa6dc2596f4cf08386e25993cea61a
Elcho911 <boss.armsport@gmail.com>
2024-01-28 01:37:23 +0600

    added react-hook-form

--- src/appPages/site/components/pages/dashboardSection/CreateCrud.tsx
+++ src/appPages/site/components/pages/dashboardSection/CreateCrud.tsx
@@ -1,29 +1,46 @@
+'use client';
 import React, { FC } from 'react';
 import scss from './CreateCrud.module.scss';
 import { Button, TextInput } from '@mantine/core';
 import { IconDatabasePlus } from '@tabler/icons-react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+type Inputs = {
+	url: string;
+	resource: string;
+};
 
 const CreateCrud: FC = () => {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors }
+	} = useForm<Inputs>();
+	const onSubmit: SubmitHandler<Inputs> = (data) => {
+		console.log(data);
+	};
+
 	return (
 		<>
 			<section className={scss.CreateCrud}>
 				<div className="container">
 					<div className={scss.content}>
 						<h1 className={scss.title}>Endpoint Information</h1>
-						<div className={scss.create_url}>
+						<form className={scss.create_url} onSubmit={handleSubmit(onSubmit)}>
 							<p className={scss.url}>
 								{process.env.NEXT_PUBLIC_API_URL}/api/v1/generateRandomToken/
 							</p>
 							<TextInput
 								className={scss.resource_input}
-								leftSectionPointerEvents="none"
 								placeholder="your resource"
+								error={!!errors.resource}
+								{...register('resource', { required: true })}
 							/>
-							<Button className={scss.button}>
+							<Button type="submit" className={scss.button}>
 								<IconDatabasePlus className={scss.icon} />
 								Create
 							</Button>
-						</div>
+						</form>
 					</div>
 				</div>
 			</section>
