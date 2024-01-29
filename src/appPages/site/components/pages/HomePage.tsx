310c8ffe0723f4b36bdcbc67af990652bb040e18
Elcho911 <boss.armsport@gmail.com>
2024-01-29 23:28:15 +0600

    update homePage

--- src/appPages/site/components/pages/HomePage.tsx
+++ src/appPages/site/components/pages/HomePage.tsx
@@ -1,27 +1,10 @@
-// 'use client';
 import React, { FC } from 'react';
-import scss from './Style.module.scss';
-// import { useGetMeQuery } from '@/redux/api/me';
 import Welcome from '@/appPages/site/components/pages/homeSection/Welcome';
 
 const HomePage: FC = () => {
-	// const { data: user, isLoading, error } = useGetMeQuery();
-	//
-	// if (!isLoading) {
-	// 	console.log('Redux', user);
-	// } else {
-	// 	console.log('Loading...');
-	// }
-
 	return (
 		<>
-			<section className={scss.home_page}>
-				<div className="container">
-					<div className={scss.content}>
-						<Welcome />
-					</div>
-				</div>
-			</section>
+			<Welcome />
 		</>
 	);
 };
