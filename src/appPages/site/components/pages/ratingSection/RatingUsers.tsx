043af655b6d92f0c27451f89b94a7a542b67644e
Elcho911 <boss.armsport@gmail.com>
2024-02-04 22:17:41 +0600

    added adaptive rating for any devices

--- src/appPages/site/components/pages/ratingSection/RatingUsers.tsx
+++ src/appPages/site/components/pages/ratingSection/RatingUsers.tsx
@@ -2,48 +2,71 @@
 import React from 'react';
 import scss from './RatingUsers.module.scss';
 import Image from 'next/image';
-import { Table } from '@mantine/core';
+import { Box, ScrollArea, Table } from '@mantine/core';
 import { useGetRatingQuery } from '@/redux/api/rating';
 import AnimatedNumbers from '../../framerMotion/AnimatedNumbers';
 
 const RatingUsers = () => {
 	const { data } = useGetRatingQuery();
-	console.log(data?.results);
 
 	const rows = data?.results.map((item, index) => (
 		<Table.Tr key={index}>
-			<Table.Td>{index + 1}</Table.Td>
+			<Table.Td>
+				{index === 0 ? (
+					<span className={scss.firstPlaceNumber}>{index + 1}</span>
+				) : index === 1 ? (
+					<span className={scss.secondPlaceNumber}>{index + 1}</span>
+				) : index === 2 ? (
+					<span className={scss.thirdPlaceNumber}>{index + 1}</span>
+				) : (
+					<span>{index + 1}</span>
+				)}
+			</Table.Td>
 			<Table.Td>
 				<div className={scss.flex_user}>
 					<Image
 						className={scss.photo}
 						src={item.photo}
-						width={25}
-						height={25}
+						width={30}
+						height={30}
 						alt={`${item.firstName} ${item.lastName}`}
 					/>
-					<span>
-						{item.firstName} {item.lastName}
-					</span>
+					{index === 0 ? (
+						<span className={scss.firstPlaceName}>
+							{item.firstName} {item.lastName}
+						</span>
+					) : index === 1 ? (
+						<span className={scss.secondPlaceName}>
+							{item.firstName} {item.lastName}
+						</span>
+					) : index === 2 ? (
+						<span className={scss.thirdPlaceName}>
+							{item.firstName} {item.lastName}
+						</span>
+					) : (
+						<span>
+							{item.firstName} {item.lastName}
+						</span>
+					)}
 				</div>
 			</Table.Td>
 			<Table.Td>
-				<AnimatedNumbers value={item.totalReq} />
+				<AnimatedNumbers value={item.totalReq!} />
 			</Table.Td>
 			<Table.Td>
-				<AnimatedNumbers value={item.get!} />
+				{item.get! ? <AnimatedNumbers value={item.get!} /> : 0}
 			</Table.Td>
 			<Table.Td>
-				<AnimatedNumbers value={item.post!} />
+				{item.post! ? <AnimatedNumbers value={item.post!} /> : 0}
 			</Table.Td>
 			<Table.Td>
-				<AnimatedNumbers value={item.put!} />
+				{item.put! ? <AnimatedNumbers value={item.put!} /> : 0}
 			</Table.Td>
 			<Table.Td>
-				<AnimatedNumbers value={item.patch!} />
+				{item.patch! ? <AnimatedNumbers value={item.patch!} /> : 0}
 			</Table.Td>
 			<Table.Td>
-				<AnimatedNumbers value={item.delete!} />
+				{item.delete! ? <AnimatedNumbers value={item.delete!} /> : 0}
 			</Table.Td>
 			<Table.Td>{item.updatedAt}</Table.Td>
 		</Table.Tr>
@@ -57,27 +80,31 @@ const RatingUsers = () => {
 						<h1 className={scss.title}>
 							Rating of <span>Users</span>
 						</h1>
-						<Table
-							striped
-							stripedColor="#181818"
-							withTableBorder
-							withColumnBorders
-						>
-							<Table.Thead>
-								<Table.Tr>
-									<Table.Th>№</Table.Th>
-									<Table.Th>User</Table.Th>
-									<Table.Th>Total Request</Table.Th>
-									<Table.Th>GET</Table.Th>
-									<Table.Th>POST</Table.Th>
-									<Table.Th>PUT</Table.Th>
-									<Table.Th>PATCH</Table.Th>
-									<Table.Th>DELETE</Table.Th>
-									<Table.Th>Last Active</Table.Th>
-								</Table.Tr>
-							</Table.Thead>
-							<Table.Tbody>{rows}</Table.Tbody>
-						</Table>
+						<ScrollArea type="always" offsetScrollbars>
+							<Box w={1240}>
+								<Table
+									striped
+									stripedColor="#181818"
+									withTableBorder
+									withColumnBorders
+								>
+									<Table.Thead>
+										<Table.Tr>
+											<Table.Th>№</Table.Th>
+											<Table.Th>User</Table.Th>
+											<Table.Th>Total Requests</Table.Th>
+											<Table.Th>GET</Table.Th>
+											<Table.Th>POST</Table.Th>
+											<Table.Th>PUT</Table.Th>
+											<Table.Th>PATCH</Table.Th>
+											<Table.Th>DELETE</Table.Th>
+											<Table.Th>Last Active</Table.Th>
+										</Table.Tr>
+									</Table.Thead>
+									<Table.Tbody>{rows}</Table.Tbody>
+								</Table>
+							</Box>
+						</ScrollArea>
 					</div>
 				</div>
 			</section>
